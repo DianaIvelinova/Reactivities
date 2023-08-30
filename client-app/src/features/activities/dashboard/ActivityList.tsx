e0e28b338a2c91e234282f757eebf971274d0a1f
Diyana <diana.ivelinova@gmail.com>
2023-08-30 16:49:03 +0300

    End of section 11

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Header } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import ActivityListItem from "./AxtivityListItem";
+import ActivityListItem from "./ActivityListItem";
 import { Fragment } from "react";
 
 export default observer(function ActivityList() {
