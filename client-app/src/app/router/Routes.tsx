6aeb8d628b7f5a0bc072653d67883636cdc5f1a7
Diyana <diana.ivelinova@gmail.com>
2023-09-04 17:31:11 +0300

    end of section 13

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,27 +1,29 @@
-import {createBrowserRouter, Navigate, RouteObject} from 'react-router-dom';
-import App from '../layout/App';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from '../../features/errors/TestError';
-import NotFound from '../../features/errors/NotFound';
-import ServerError from '../../features/errors/ServerError';
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
+import App from "../layout/App";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
+import LoginForm from "../../features/users/LoginForm";
 
 export const routes: RouteObject[] = [
-    {
-        path: '/',
-        element: <App />, 
-        children: [
-            {path: 'activities', element: <ActivityDashboard/>},
-            {path: 'activities/:id', element: <ActivityDetails/>},
-            {path: 'createActivity', element: <ActivityForm key='create'/>},
-            {path: 'manage/:id', element: <ActivityForm key='manage'/>},
-            {path: 'errors', element: <TestErrors />},
-            {path: 'not-found', element: <NotFound />},
-            {path: 'server-error', element: <ServerError />},
-            {path: '*', element: <Navigate replace to='not-found' />}
-        ]
-    }
-]
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "activities/:id", element: <ActivityDetails /> },
+      { path: "createActivity", element: <ActivityForm key="create" /> },
+      { path: "manage/:id", element: <ActivityForm key="manage" /> },
+      { path: "login", element: <LoginForm /> },
+      { path: "errors", element: <TestErrors /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "*", element: <Navigate replace to="not-found" /> },
+    ],
+  },
+];
 
 export const router = createBrowserRouter(routes);
